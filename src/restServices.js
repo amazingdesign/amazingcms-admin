f820992faed141755828a9499866c02c1beceac6
Mateusz <chomamateusz@gmail.com>
2020-01-02 11:51:24 +0100

    Reloading system and normal collections after any modyfications

--- src/restServices.js
+++ src/restServices.js
@@ -19,6 +19,21 @@ const updateSystemCollectionsOnRecordModify = ({ method, name }, dispatch) => {
   }
 }
 
+const updateCollectionsOnRecordModify = ({ method, name }, dispatch) => {
+  if (['update', 'create', 'delete'].includes(name)) {
+    // need to reload all collections after change
+    const currentInstance = instances[0]
+    dispatch(currentInstance.actions.collections.find())
+  }
+}
+
+const updateAllCollectionsOnRecordModify = (...all) => {
+  return Promise.all([
+    updateSystemCollectionsOnRecordModify(...all),
+    updateCollectionsOnRecordModify(...all),
+  ])
+}
+
 const servicesDeclarations = [
   {
     name: 'system-collections',
@@ -35,66 +50,61 @@ const servicesDeclarations = [
     name: 'actions',
     url: `${window._env_.REACT_APP_API_URL}/actions/:collectionName/:id`,
     actionsDeclarations: crudActionsDeclarations,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-users',
     url: `${window._env_.REACT_APP_API_URL}/users/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-groups',
     url: `${window._env_.REACT_APP_API_URL}/groups/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-privileges',
     url: `${window._env_.REACT_APP_API_URL}/privileges/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-orders',
     url: `${window._env_.REACT_APP_API_URL}/orders/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-payments',
     url: `${window._env_.REACT_APP_API_URL}/payments/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-events',
     url: `${window._env_.REACT_APP_API_URL}/events/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-events-log',
     url: `${window._env_.REACT_APP_API_URL}/events-log/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-languages',
     url: `${window._env_.REACT_APP_API_URL}/languages/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: updateSystemCollectionsOnRecordModify,
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'system-collection-collections',
     url: `${window._env_.REACT_APP_API_URL}/collections/:id`,
     actionsDeclarations: crudActionsDeclarations,
-    onReceivesData: ({ method, name }, dispatch) => {
-      if (['update', 'create', 'delete'].includes(name)) {
-        // need to reload all collections after change
-        const currentInstance = instances[0]
-        dispatch(currentInstance.actions.collections.find())
-      }
-    },
+    onReceivesData: updateAllCollectionsOnRecordModify,
   },
   {
     name: 'uploader',
