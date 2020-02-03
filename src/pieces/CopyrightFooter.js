a114841a240a805205b0a845701594052c0986f1
Mateusz <chomamateusz@gmail.com>
2020-02-03 13:58:50 +0100

    Target _blank in footer link

--- src/pieces/CopyrightFooter.js
+++ src/pieces/CopyrightFooter.js
@@ -14,7 +14,7 @@ const CopyrightFooter = (props) => (
       {/* eslint-disable-next-line max-len */}
       <path d="M22 7v10h-20v-10h20zm2-2h-24v14h24v-14zm-18 3h-3v2h3v-2zm3 0h-2v2h2v-2zm3 0h-2v2h2v-2zm3 0h-2v2h2v-2zm3 0h-2v2h2v-2zm3 0h-2v2h2v-2zm-4 6h-10v2h10v-2zm4-3h-4v2h4v-2zm-14 0h-4v2h4v-2zm3 0h-2v2h2v-2zm3 0h-2v2h2v-2zm3 0h-2v2h2v-2z"></path>
     </svg>
-    by <a href="https://amazingdesign.eu/">Amazing Design</a>
+    by <a href="https://amazingdesign.eu/" target="_blank" rel="noopener noreferrer">Amazing Design</a>
   </div>
 )
 
